005657fc25b8c5cf5dec84969bb203a0cc245b58
Sk Zaffar Ekbal <skzaffar.ekbal@nettantra.net>
2022-10-04 20:11:45 +0530

    Handling validation and wrong id error.

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -7,6 +7,18 @@ export default (err, req, res, next) => {
 
   error.message = err.message;
 
+  // Wrong Mongoose Object ID Error
+  if (err.name === "CastError") {
+    const message = `Resourse not found. Invalid: ${err.path}`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  // Handling mongoose Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     error,
